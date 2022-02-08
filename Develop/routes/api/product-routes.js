102ea3febc4a796f69d5b8c421f3d713d78ea3d9
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-02-07 22:19:13 -0500

    updated routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,13 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findALL({
+      include: [{model:Tag}, {model:Tag}]
+    });
+    res.status(200).json(productData);
+  } catch(err){
+    res.status(500).json(err)
+  }
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  try{
+    const productData = await Product.findByPk(req.params.id,{
+      include: [{model: Category}, {model:Tag}],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'Cannot find id'});
+      return
+    }
+      res.status(200).json(productData);
+    } catch(err){
+      res.status(500).json(err);
+    }
+  });
 
 // create new product
 router.post('/', (req, res) => {
@@ -25,6 +46,8 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -90,7 +113,23 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  try {const productData = await Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  });
+
+  if (!productData){
+    res.status(404).json({ message: 'No  id found!' });
+    return;
+  }
+  res.status(200).json(productData);
+
+} 
+catch (err) {
+  res.status(500).json(err);
+}
+
 });
 
 module.exports = router;
