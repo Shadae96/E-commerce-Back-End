588818bf408957f66beb4046f4c94856fe6ecfee
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-02-09 23:19:29 -0500

    fixing database connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require ('./config/connection')
+const sequelize = require ('./config/connection');
+
+const seedTags = require('./seeds/tag-seeds')
+const seedProductTags = require('./seeds/product-tag-seeds')
+const seedProducts = require('./seeds/product-seeds')
+const seedCategories = require('./seeds/category-seeds')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,8 +15,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
 // sync sequelize models to the database, then turn on the server
+
+sequelize.sync({ force: true}).then(()=>{
+
+seedCategories();
+seedProducts();
+seedProductTags();
+seedTags();
+
+}).then(()=> {
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+})
 });
